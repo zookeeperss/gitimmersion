4a49191e7dcf5cc5c37c0828a2a9382dd3ef034b
lianpengju <15810667197@163.com>
2016-07-21 14:51:37 +0800

    Moved hello.rb to lib

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,5 +1,9 @@
+<<<<<<< HEAD
 # Default is "World"
 # Author: lianpengju(15810667197@163.com)
+=======
+# This is an unwanted but committed change
+>>>>>>> 1378312e10abc2621f07ed4a3eaf77c3cf7b04e6
 name = ARGV.first || "World"
 
 puts "Hello,#{name}!"
